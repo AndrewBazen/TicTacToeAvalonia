6e09e2113c9dde6847bdf6efe8b041e24f778cec
Chris Gosseen <185705028+cgosseen@users.noreply.github.com>
2025-04-23 11:02:14 -0400

    Initial morning commit.  Shows both methods Chris and Andrew applied for the tictactoe board

--- TicTacToeAvalonia/MainWindow.axaml.cs
+++ TicTacToeAvalonia/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System.Collections.Generic;
 
 namespace TicTacToeAvalonia
 {
@@ -7,6 +8,71 @@ namespace TicTacToeAvalonia
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public class Board()
+        {
+            //fields
+            //Tuple + Dictionary option:
+            //private List<(int, int)> _boardGrid = new List<(int, int)>
+            //    { 
+            //    (0, 0), 
+            //    (0, 1), 
+            //    (0, 2), 
+            //    (1, 0), 
+            //    (1, 1), 
+            //    (1, 2), 
+            //    (2, 0), 
+            //    (2, 1), 
+            //    (2, 2) 
+            //    };
+
+
+            //Grid option
+            public int[][] boardGrid { get; private set; } = new int[9][]
+           {
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 0],
+            [1, 1],
+            [1, 2],
+            [2, 0],
+            [2, 1],
+            [2, 2],
+           };
+
+            //properties
+            //Tuple + dictionary option
+            public Dictionary<(int, int), PieceType> TicTacToeBoard { get; set; } = new(); // creates getter/setter while initializing
+
+            //Grid option dictionary
+            public Dictionary<int[], PieceType> boardStates = new Dictionary<int[], PieceType>();
+            //Methods
+
+            //Constructor
+            public Board()
+            {
+                //Tuple + Dictionary Option
+                //foreach((int, int) cell in _boardGrid)  //initializes each cell to EmptySpace
+                //{
+                //    TicTacToeBoard.Add(cell, PieceType.EmptySpace);
+                //}
+
+                //Grid option
+                for (int i = 0; i < 9; i++)
+                {
+                    boardStates[boardGrid[i]] = PieceType.EmptySpace;
+                }
+
+            }
+        }
+
+        public enum PieceType
+        {
+            XPiece,
+            OPiece,
+            EmptySpace
         }
     }
 }
--- TicTacToeTest/Test1.cs
+++ TicTacToeTest/Test1.cs
@@ -1,4 +1,6 @@
-﻿namespace TicTacToeTest
+﻿using static TicTacToeAvalonia.MainWindow;
+
+namespace TicTacToeTest
 {
     [TestClass]
     public sealed class Test1
@@ -6,6 +8,12 @@
         [TestMethod]
         public void TestMethod1()
         {
+            Board board = new Board();
+
+            Assert.IsTrue(board.boardStates.Count == 9, "The board should have 9 states.");
+            Assert.IsTrue(board.boardStates[board.boardGrid[0]] == PieceType.EmptySpace, "The first cell should be empty.");
+
+
         }
     }
 }
