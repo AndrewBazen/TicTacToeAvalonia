863698fda072817c3ba15c8ebf246f369fb0dfa2
isaac choi <104857720+cvvsi@users.noreply.github.com>
2025-04-23 09:49:25 -0400

    tic tac toe init

---
+++ TicTacToe.cs
@@ -0,0 +1,28 @@
+public enum PieceType
+{
+    EmptySpace,
+    XPiece,
+    OPiece
+}
+
+// game board for Tic Tac using a dictionary
+public class GameBoard
+{
+    // initialize dictionary to store game pieces
+    // key is pair (row, col) and value is the piece (XPiece, OPiece, EmptySpace)
+    public Dictionary<(int, int), PieceType> Cells { get; private set }
+
+    // initalize how many rows and columns the tic tac toe board has (3x3)
+    public int Rows { get; }
+    public int Cols { get; }
+
+    // constructor: runs when we create new GameBoard
+    // initalizes each cell to be empty at first and sets up 3x3 board
+    public GameBoard(int rows=3, int cols = 3)
+    {
+        // stores the number of rows/cols
+        Rows = rows;
+        Cols = cols;
+        Cells = new Dictionary<(int, int), PieceType>();
+    }
+}
